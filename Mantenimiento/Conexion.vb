9ed134d63d7e6280d0a221759214c5e99fa84ffe
Miguel <miguel.alvarado0807@gmail.com>
2021-01-27 06:35:28 -0600

    Inicio de modificacion

--- Mantenimiento/Conexion.vb
+++ Mantenimiento/Conexion.vb
@@ -9,6 +9,7 @@
     Public conexion As String = "Data Source=tcp:COS-F4Z1XQ2\SQLEXPRESS,49500;Initial Catalog=Inventario; User Id=malvarado; Password=1234Admin" 'Choi
 
 
+
     Public cn As New SqlClient.SqlConnection(conexion)
 
         Sub conectar()
