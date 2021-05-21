154c14e3e768166769e4687d5e7ed76b3a2e8381
miguel0807 <miguel.alvarado0807@gmail.com>
2021-05-21 04:38:54 -0600

    login

---
+++ Mantenimiento/Common/Cache/ActiveUser.vb
@@ -0,0 +1,12 @@
+﻿Public Module ActiveUser
+    Public idUser
+    Public loginName
+    Public password
+    Public firstName
+    Public lastName
+    Public position
+    Public email
+    Public OrdenProduccion
+
+End Module
+
