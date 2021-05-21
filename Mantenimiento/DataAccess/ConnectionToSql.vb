63f518dd71a815703e90f5c078735909d2300480
miguel0807 <miguel.alvarado0807@gmail.com>
2021-05-21 04:37:59 -0600

    login

---
+++ Mantenimiento/DataAccess/ConnectionToSql.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Data.SqlClient
+Public MustInherit Class ConnectionToSql
+    Private connectionString As String
+    Protected Sub New()
+
+        'connectionString = "Data Source=MAT-COS-DB-10Z2\SQLEXPRESS;Initial Catalog=Usuarios;integrated security=true" 'Miguel
+        connectionString = "Data Source=tcp:COS-F4Z1XQ2\SQLEXPRESS,49500;Initial Catalog=Usuarios; User Id=malvarado; Password=1234Admin" 'Choi
+
+    End Sub
+    Protected Function GetConnection() As SqlConnection
+        Return New SqlConnection(connectionString)
+    End Function
+End Class
---
+++ Mantenimiento/DataAccess/UserDao.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports Common
+
+Public Class UserDao
+    Inherits ConnectionToSql
+    Public Function Login(user As String, pass As String) As Boolean
+        Using connection = GetConnection()
+            connection.Open()
+            Using command = New SqlCommand()
+                command.Connection = connection
+                command.CommandText = "select *from users where loginName=@user and password=@pass"
+                command.Parameters.AddWithValue("@user", user)
+                command.Parameters.AddWithValue("@pass", pass)
+                command.CommandType = CommandType.Text
+                Dim reader = command.ExecuteReader()
+                If reader.HasRows Then
+                    While reader.Read()
+                        ActiveUser.idUser = reader.GetInt32(0)
+                        ActiveUser.loginName = reader.GetString(1)
+                        ActiveUser.password = reader.GetString(2)
+                        ActiveUser.firstName = reader.GetString(3)
+                        ActiveUser.lastName = reader.GetString(4)
+                        ActiveUser.position = reader.GetString(5)
+                        ActiveUser.email = reader.GetString(6)
+
+                    End While
+                    reader.Dispose()
+
+
+                    Return True
+                Else
+                    Return False
+                End If
+            End Using
+        End Using
+    End Function
+End Class
