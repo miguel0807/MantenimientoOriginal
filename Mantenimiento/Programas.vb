30c9d6de8d2213b91a9c40d4c07e30c4d5d40b16
miguel0807 <miguel.alvarado0807@gmail.com>
2021-06-09 14:53:18 -0600

    Prototipo plc

--- Mantenimiento/Programas.vb
+++ Mantenimiento/Programas.vb
@@ -39,12 +39,12 @@
         Me.Close()
     End Sub
 
-    Private Sub Programas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Programas_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
 
 
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles button2.Click
+        Dim f As New Proyecto.FormPrincipal()
+
+        f.Show()
+        Me.Close()
     End Sub
 End Class
